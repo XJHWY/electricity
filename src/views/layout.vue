b7ab8d766918481f4c2d392289c200dee4fac780
JOJO <1654441515@qq.com>
2022-07-27 10:39:46 +0800

    '初始化项目'

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,11 +1,19 @@
 <template>
 <div>
-  我是你爹
+
 </div>
 </template>
 
 <script>
 export default {
+  created () {},
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
   components: {}
 }
 </script>
